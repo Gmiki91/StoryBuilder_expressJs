96245eddb5374da044a121e22452fd71586eda17
Miklos Gabor <gmiki91@gmail.com>
2022-02-22 13:05:52 +0100

    ownstorycheck added to pagedelete

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -3,16 +3,17 @@ const router = express.Router();
 const pageController = require('../controllers/pageController');
 const authCheck = require('../middleware/authCheck');
 const langInfo = require('../middleware/langInfo');
+const ownStoryCheck = require('../middleware/ownStoryCheck');
 
 router.post('/', authCheck, pageController.createPage);
 
 router.route('/:id')
     .get(pageController.getPage)
-    .delete(authCheck, pageController.deletePage)
+    .patch(authCheck,ownStoryCheck, pageController.deletePage)
     
 router.route('/many/:ids')
     .get(pageController.getPages)
-    .delete(authCheck, pageController.deletePages)
+    .patch(authCheck,ownStoryCheck, pageController.deletePages)
 
 router.put('/rateText', authCheck, pageController.rateText);
 router.get('/all/:authorId', authCheck,langInfo, pageController.getPageDataByAuthor);
